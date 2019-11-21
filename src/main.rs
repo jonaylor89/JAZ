4a906c5ffee9470b4453d44192e9c2392cfa1f82
John Naylor <jonaylor89@gmail.com>
2019-11-21 17:25:32 -0500

    iterate branches

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 
-use git2::Repository;
+use git2::{Repository, BranchType};
 
 fn main() {
 
+    // Get path to git repo via command line args or assume current directory
     let repo_root = std::env::args().nth(1).unwrap_or(".".to_string());
 
+    // Open git repo
     let repo = Repository::open(repo_root.as_str()).expect("Couldn't open repository");
 
-    println!("{} state={:?}", repo.path().display(), repo.state());
+    for branch in repo.branches(Some(BranchType::Local)).unwrap() {
+
+        // This is not what rust code should look like
+        println!("[INFO] Scanning branch {}", branch.unwrap().0.name().unwrap().unwrap());
+    }
+
+    // Print the current start of the git repo
+    println!("[INFO] {} state={:?}", repo.path().display(), repo.state());
 
 }
