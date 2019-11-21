136075279cf57c02ce78f5bb038d2e5a7d7afffd
John Naylor <jonaylor89@gmail.com>
2019-11-21 13:12:23 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+use git2::Repository;
+
 fn main() {
-    println!("Hello, world!");
+
+
+    let url = "https://github.com/alexcrichton/git2-rs";
+    let repo = match Repository::clone(url, "/Users/johannes/Repos/git2") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to init: {}", e),
+    };
+
+
+
 }
