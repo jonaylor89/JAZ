0debfc73090424f46a863f4f279abbfdeb7f8b64
John Naylor <jonaylor89@gmail.com>
2019-11-21 18:43:22 -0500

    color

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-
-use git2::{Repository, BranchType};
-// use serde_json::json;
-use serde_json;
-use std::fs;
-use std::collections::HashMap;
+use git2::{BranchType, Repository};
 use regex::Regex;
+use serde_json;
+use std::collections::HashMap;
+use std::fs;
+use termion::color;
 
 const CONFIG_FILE: &str = "rules.json";
 
@@ -15,10 +14,6 @@ fn main() {
 
     // Make a hashmap of uncompiled regex expressions
     let conf: HashMap<String, String> = serde_json::from_str(&conf_str).unwrap();
-
-    // for (key, val) in &conf {
-    //     println!("{}: \"{}\"", key, val);
-    // }
 
     // Get path to git repo via command line args or assume current directory
     let repo_root = std::env::args().nth(1).unwrap_or(".".to_string());
@@ -32,19 +27,19 @@ fn main() {
 
     for (key, val) in &conf {
         let re = Regex::new(val).unwrap();
-        
         if re.is_match(test) {
-            println!("[CRITIAL] there is a cred of type `{}` in the repo", key)
+            println!("{}[CRITIAL]{} there is a cred of type `{}` in the repo", color::Fg(color::Red), color::Fg(color::Reset), key)
         }
     }
 
     for branch in repo.branches(Some(BranchType::Local)).unwrap() {
-        
         // This is not what rust code should look like
-        println!("[INFO] Scanning branch {}", branch.unwrap().0.name().unwrap().unwrap());
+        println!(
+            "[INFO] Scanning branch {}",
+            branch.unwrap().0.name().unwrap().unwrap()
+        );
     }
 
     // Print the current start of the git repo
     println!("[INFO] {} state={:?}", repo.path().display(), repo.state());
-
 }
