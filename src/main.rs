9bcbe93f97f8b25f88a29d6c332468296285eb79
Johannes Naylor <jonaylor89@gmail.com>
2022-01-25 20:16:52 -0500

    clippy

--- src/main.rs
+++ src/main.rs
@@ -98,7 +98,7 @@ mod tests {
     #[test]
     fn find_nothing() {
 
-        let secret: &[u8] = &"Nothing to see here".as_bytes();
+        let secret: &[u8] = "Nothing to see here".as_bytes();
         let result: Option<Vec<& 'static str>> = find_secrets(secret);
 
         assert!(result.is_none());
@@ -107,9 +107,9 @@ mod tests {
     #[test]
     fn find_ssh_key() {
 
-        let secret: &[u8] = &"-----BEGIN OPENSSH PRIVATE KEY-----".as_bytes();
+        let secret: &[u8] = "-----BEGIN OPENSSH PRIVATE KEY-----".as_bytes();
         let result: Vec<& 'static str> = find_secrets(secret).expect("Should find a secret");
 
-        assert_eq!(result.iter().next().expect("Should contain one secret type"), &"SSH (OPENSSH) private key");
+        assert_eq!(result.get(0).expect("Should contain one secret type"), &"SSH (OPENSSH) private key");
     }
 }
